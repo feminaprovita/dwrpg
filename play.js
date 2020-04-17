4f8bbaf2bed1dee32c290da013eb9d7cdb9c6e66
Claire Gilligan <feminaprovita@gmail.com>
2020-04-17 19:17:09 -0400

    DRYer code

--- play.js
+++ play.js
@@ -15,40 +15,15 @@ console.log(' ')
 console.log(`~* ${thisRound} *~`)
 
 // for each, selects and prints a randomly selected category (numbered)
-let rand = getRandomInt(seed.length)
-console.log('1. ', seed[rand])
+const playGame = num => {
+  let rand
+  for(let i = 1; i <= num; i++) {
+    rand = getRandomInt(seed.length)
+    console.log(`${i}. ${seed[rand]}`)
+  }
+}
 
-rand = getRandomInt(seed.length)
-console.log('2. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('3. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('4. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('5. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('6. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('7. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('8. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('9. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('10. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('11. ', seed[rand])
-
-rand = getRandomInt(seed.length)
-console.log('12. ', seed[rand])
+// a standard game of Scattergories has 12 categories
+playGame(12)
 console.log(' ')
 // extra line at the top and bottom of each round for non-dev readability in console
