92668edbd2bb50a96b5806bc8b07ff01a06d71e9
Claire Gilligan <feminaprovita@gmail.com>
2020-05-17 15:31:00 -0400

    fixed repeats

--- README.md
+++ README.md
@@ -13,13 +13,14 @@ Designed for screenshare on family zoom calls.
 2. Set up screen to share, split between terminal window and timer (set for 3-5
 minutes per round, your choice).
 3. Run `node play.js` in the terminal.
-4. Eyeball list to confirm there are no duplicates†, and you're willing to play with
-this letter‡, and start the timer.
+4. Eyeball list to confirm there are no thematic duplicates†, and you're willing
+to play with this letter‡, and start the timer.
 5. Play [Scattergories](https://en.wikipedia.org/wiki/Scattergories#Gameplay)!
 
-† This script does not remove categories from the database after they've printed
-to the terminal, so there's a chance of the same category being repeated in the same
-dozen. Just initiate a new game.
+† This script is not intelligent; it's just a randomizer, and as such it can prevent
+"Things that are cold" from coming up twice in one round, but it cannot prevent
+"Fictional characters" and "Literary characters" from coming up together. Just
+initiate a new game.
 
 ‡ The original game uses a 20-sided die, and as such eliminates the following letters:
 QUVXYZ. I have left them in, but if you want to remove them, simply remove the `/*`
--- play.js
+++ play.js
@@ -1,18 +1,13 @@
 const seed = require('./seed')
 
-
+// simple randomizer
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
+// chooses a random letter (includes full alphabet -- see bottom of file)
 const randomLetter = () => {
   const asciiLow = 65
   const asciiHigh = 90
   let randomAscii = Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow)
-
-// comment in the lines immediately below to remove QUVXYZ, as in the original game
-  /* const forbiddenLetters = [81, 85, 86, 88, 89, 90]
-  while(forbiddenLetters.includes(randomAscii)) {
-    randomAscii = Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow)
-  } */
   return String.fromCharCode(randomAscii)
 }
 
@@ -24,7 +19,7 @@ console.log(`~* ${thisRound} *~`)
 // this will hold the indexes of the categories already used in this round
 const game = []
 
-// for each, selects and prints a randomly selected category (numbered)
+// for each round, selects and prints a randomly selected category (numbered)
 const playGame = num => {
   let rand
   for(let i = 1; i <= num; i++) {
@@ -36,7 +31,24 @@ const playGame = num => {
   }
 }
 
-// a standard game of Scattergories has 12 categories
+// a standard round of Scattergories has 12 categories
 playGame(12)
 console.log(' ')
 // extra line at the top and bottom of each round for non-dev readability in console
+
+
+
+
+/* *****************************************************************************
+// to remove QUVXYZ, as in the original game, replace the formula in lines 7-12
+// with the following (without the comment formatting)
+const randomLetter = () => {
+  const asciiLow = 65
+  const asciiHigh = 87
+  let randomAscii = Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow)
+  const forbiddenLetters = [81, 85, 86]
+  while(forbiddenLetters.includes(randomAscii)) {
+    randomAscii = Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow)
+  }
+  return String.fromCharCode(randomAscii)
+} */
