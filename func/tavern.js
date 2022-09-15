ede1b953b71e9dd6e44d6dea915128d5d67f0dfb
Claire Rogel <feminaprovita@gmail.com>
2022-09-15 16:29:17 -0400

    taverns, other updates

--- func/tavern.js
+++ func/tavern.js
@@ -1,8 +1,6 @@
 const boasts = require('../seed/tavern-boasts')
 const drinks = require('../seed/tavern-drinks')
 const foods = require('../seed/tavern-foods')
-const names = require('../seed/tavern-names')
-const owners = require('../seed/tavern-owners')
 const enc = require('../seed/taverncounters')
 const stories = require('../seed/tavern-stories')
 
@@ -13,8 +11,6 @@ const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 const tavern = {}
 
 const generateTavern = () => {
-    // tavern.name = names[getRandomInt(names.length)]
-    // tavern.owner = owners[getRandomInt(owners.length)]\
     console.log('https://www.dndspeak.com/tavern-generator/')
     console.log('https://donjon.bin.sh/fantasy/inn/')
     console.log('https://www.dndspeak.com/carousing-generator/)')
