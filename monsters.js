05ff1ffab7ca2e1d0e1d53033166fc587da32e64
Claire Rogel <feminaprovita@gmail.com>
2022-09-19 22:07:54 -0400

    fleshing out session zero npcs

---
+++ monsters.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+// Amorphous (monster): Its anatomy is bizarre and unnatural.
+// Cautious (monster): It prefers the defensive and will avoid a fight unless it is sure to win.
+// Construct (monster): It was made, not born.
+// Devious (monster): The real threat of it isn’t in a bloody fight.
+// Group (monster organization): It’s usually encountered in groups of 3–6.
+// Hoarder (monster) It collects valuable things. This tag also gives a bonus on tandom treasure rolls.
+// Horde (monster organization): It’s usually encountered in groups of more than 7, maybe many more.
+// Huge (monster size): It’s as big as a small house or larger.
+// Intelligent (monster): It’s about as smart as a person.
+// Large (monster size); It’s bigger than a human, up to the size of a cart.
+// Magical (monster): It’s beyond the physical in some way. You can’t explain it with the laws of nature.
+// Organized (monster): Groups of this monster will have formal organization—if you kill one, others might seek revenge.
+// Planar (monster): It’s not from this world.
+// Small (monster size): It’s about halfling size.
+// Solitary (monster organization): It can live and fight without the help of others. This tag gives a bonus to random treasure rolls.
+// Stealthy (monster): It can avoid detection and attack with surprise.
+// Terrifying (monster): Just being close to it is likely to make people break and flee.
+// Tiny (monster size): It’s smaller than a halfling.
